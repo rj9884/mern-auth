db54e2b872b01e8a9574197b0efe6ec8d0a1469f
RAJAN JAISWAL <rj.vidyagyan@gmail.com>
2025-08-07 19:23:57 +0530

    Update login

--- server/src/controller/auth.controller.js
+++ server/src/controller/auth.controller.js
@@ -24,8 +24,8 @@ const register = async (req, res) => {
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+            secure: true,
+            sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
@@ -85,8 +85,8 @@ const logout = async (req, res) => {
     try {
         res.clearCookie("token", {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+            secure: true,
+            sameSite: "None",
 
         }
         )
