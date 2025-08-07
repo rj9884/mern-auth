15e214ff900eb6cc38f20ca3d05811719fd5e58f
RAJAN JAISWAL <rj.vidyagyan@gmail.com>
2025-08-07 21:23:30 +0530

    update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -12,6 +12,9 @@ app.use(cors({
 app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended: true, limit:"16kb"}));
 
+app.get('/ping', (req, res) => {
+  res.send('pong');
+});
 app.get("/", (req, res)=> res.send("Hey there!"));
 import authRouter from './routes/auth.route.js'
 app.use('/api/v1/auth',authRouter);
--- server/src/index.js
+++ server/src/index.js
@@ -4,9 +4,9 @@ import { app } from "./app.js";
 
 connectDB()
 .then(()=>{
-    const port = process.env.PORT || 5000;
+    const port = process.env.PORT || 8000;
     app.listen(port, ()=>{
-        console.log(`Server is running at ${process.env.HOST || 'localhost'}:${port}`);
+        console.log(`Server is running on port ${port}`);
     })
 })
 .catch((err)=>{
