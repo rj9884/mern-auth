959584e816bb8f6960d244b172b75cce6f9c74f6
RAJAN JAISWAL <rj.vidyagyan@gmail.com>
2025-08-08 00:12:05 +0530

    file update

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://mern-auth-68hj.onrender.com',
-        changeOrigin: true,
-      },
-    },
-  },
   plugins: [react(), tailwindcss()],
 })
