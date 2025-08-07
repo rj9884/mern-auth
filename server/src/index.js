1bd5f5ad12fcc537f98e384ab1539fc17b19bdde
RAJAN JAISWAL <rj.vidyagyan@gmail.com>
2025-08-07 21:29:31 +0530

    file update

--- server/src/index.js
+++ server/src/index.js
@@ -3,12 +3,17 @@ import connectDB from "./db/connectDB.js";
 import { app } from "./app.js";
 
 connectDB()
-.then(()=>{
-    const port = process.env.PORT || 8000;
-    app.listen(port, ()=>{
-        console.log(`Server is running on port ${port}`);
+    .then(() => {
+        const port = process.env.PORT;
+        console.log("🔍 process.env.PORT =", process.env.PORT);
+        if (!port) {
+            throw new Error("❌ PORT is not defined. Are you running this in Render?");
+        }
+        
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        })
     })
-})
-.catch((err)=>{
-    console.log("Database Connection Failed!!",err);
-})
+    .catch((err) => {
+        console.log("Database Connection Failed!!", err);
+    })
